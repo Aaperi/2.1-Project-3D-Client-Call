4cc8b996e8deb9a691c5d66be37b53f109df6f4d
VladislavVidenov <vladislavvidenov95@gmail.com>
2015-09-03 14:49:06 +0200

    Update 0.3
    Movement working
    Picking object working.
    Camera feature implemented.
    Rigidbodies applied.

--- Project 3D/Assets/Scripts/BigBroMovement.cs
+++ Project 3D/Assets/Scripts/BigBroMovement.cs
@@ -1,50 +1,58 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
 
-public class BigBroMovement : MonoBehaviour {
-	//Components
-	//private Camera _playerCamera; 
-	private CharacterController _controller;
+public class BigBroMovement : MonoBehaviour
+{
+    Camera cam;
+    Rigidbody rigibody;
+    public float speed = 10.0f;
+    float gravity = 10.0f;
+    float maxVelocityChange = 10.0f;
 
-	//Variables
-	private Vector3 _input;
-	private Vector3 _movement;
-	private Quaternion _targetRotation;
 
-	[SerializeField]
-	private float _walkSpeed = 100;
-	[SerializeField]
-	float gravity = 20.0f;
-	[SerializeField]
-	float jumpHeight = 8.0f;
-	
-	void Start () {
-		_controller = GetComponent<CharacterController>();
+    void Start()
+    {
+        cam = Camera.main;
+        rigibody = GetComponent<Rigidbody>();
+    }
 
-	}
-	
-	void Update () {
+    void FixedUpdate()
+    {
+       Vector3 input = new Vector3(Input.GetAxisRaw("HorizontalB"), 0, Input.GetAxisRaw("VerticalB"));
 
-		PlayerMovement ();
 
-	}
-	
-	void PlayerMovement(){
+        Vector3 lookdir = cam.transform.forward;
+        lookdir.y = 0;
+        lookdir.Normalize();
 
-		if (_controller.isGrounded) {
-			_input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
-			_movement = _input;
-			_movement *= (Mathf.Abs (_input.x) == 1 && Mathf.Abs (_input.z) == 1) ? 0.7f : 1;
-			_movement = transform.TransformDirection(_movement);
-			_movement *= _walkSpeed;
+        Vector3 targetVelocity = Vector3.zero;
+        targetVelocity += lookdir * input.z;
+        targetVelocity -= cam.transform.right * -input.x;
 
-			if (Input.GetButton ("Jump")) {
-				_movement.y = jumpHeight;
-			}
-		}
-		_movement.y -= gravity * Time.deltaTime;
-		_controller.Move (_movement * Time.deltaTime);
-	}
+       
+
+        targetVelocity *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? 0.7f : 1;
+
+       // targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= speed;
+        if (targetVelocity.magnitude > 0)
+        {
+            this.transform.rotation = Quaternion.LookRotation(new Vector3(targetVelocity.x, 0, targetVelocity.z));
+        }
+            // Apply a force that attempts to reach our target velocity
+        Vector3 velocity = rigibody.velocity;
+        Vector3 velocityChange = (targetVelocity - velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+
+        rigibody.AddForce(velocityChange, ForceMode.VelocityChange);
+       
+        // We apply gravity manually for more tuning control
+        rigibody.AddForce(new Vector3(0, -gravity * rigibody.mass, 0));
+    }
+    
 
 }
